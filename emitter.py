bb51aed3edb83d181148605ac3f270b13222a9a9
Joao Martins <joaofbsm@hotmail.com>
2017-06-11 14:51:35 -0300

    Add modules structure and initial documentation

---
+++ emitter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Messaging System Client Messages Emmiter"""
+
+import sys
+import socket
+
+__author__ = "João Francisco Martins and Vitor Bernardo Jorge"
---
+++ server.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Event-driven Messaging System Server"""
+
+import sys
+import socket
+
+__author__ = "João Francisco Martins and Vitor Bernardo Jorge"
